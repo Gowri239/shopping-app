8aed4702fb66c073562c74aa9eaf9e9f506a30de
Gowri239 <gowrimopuru14@gmail.com>
2022-10-04 22:25:46 +0530

    user-product relation

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,18 +13,18 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
+  req.user.CreateProduct({
     title: title,
     price: price,
     imageUrl: imageUrl,
     description: description
   })
-    .then(result => {
+  .then(result => {
       // console.log(result);
       console.log('Created Product');
       res.redirect('/admin/products');
     })
-    .catch(err => {
+  .catch(err => {
       console.log(err);
     });
 };
@@ -35,7 +35,8 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findByPk(prodId)
+  req.user
+    .getProducts({ where: {id: prodId}})
     .then(product => {
       if (!product) {
         return res.redirect('/');
@@ -72,7 +73,8 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  req.user
+    .getProducts()
     .then(products => {
       res.render('admin/products', {
         prods: products,
